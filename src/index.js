2e5a6f819f6918f14d081ae92073629c1ed31a02
Joe McWhorter <joseph.s.mcwhorter@gmail.com>
2020-02-11 14:00:35 -0500

    Adds reducers

--- src/index.js
+++ src/index.js
@@ -15,6 +15,16 @@ import { createStore, combineReducers } from 'redux';
 
 // global action types
 
+const ADD_BOOK = "ADD_BOOK";
+const ADD_MUSIC = "ADD_MUSIC";
+const ADD_MOVIE = "ADD_MOVIE";
+const REM_BOOK = "REM_BOOK";
+const REM_MUSIC = "REM_MUSIC";
+const REM_MOVIE = "REM_MOVIE";
+const UPDATE_BOOK = "UPDATE_BOOK";
+const UPDATE_MUSIC = "UPDATE_MUSIC";
+const UPDATE_MOVIE = "UPDATE_MOVIE";
+
 // basic states
 
 const bookState = [{
@@ -37,6 +47,62 @@ const movieState = [{
 }]
 
 // reducers
+
+function bookManager(state=bookState, action) {
+    newState = [...state];
+
+    switch (action.type) {
+        case ADD_BOOK:
+            newState.push(action.payload);
+            break;
+        case REM_BOOK:
+            newState.splice(action.id, 1);
+            break;
+        case UPDATE_BOOK:
+            newState[action.id] = action.payload;
+        default:
+            break;
+    }
+
+    return newState;
+}
+
+function musicManager(state=musicState, action) {
+    newState = [...state];
+
+    switch (action.type) {
+        case ADD_MUSIC:
+            newState.push(action.payload);
+            break;
+        case REM_MUSIC:
+            newState.splice(action.id, 1);
+            break;
+        case UPDATE_MUSIC:
+            newState[action.id] = action.payload;
+        default:
+            break;
+    }
+
+    return newState;
+}
+
+function bookManager(state=movieState, action) {
+    newState = [...state];
+
+    switch (action.type) {
+        case ADD_MOVIE:
+            newState.push(action.payload);
+            break;
+        case REM_MOVIE:
+            newState.splice(action.id, 1);
+            break;
+        case UPDATE_MOVIE:
+            newState[action.id] = action.payload;
+        default:
+            break;
+    }
+    return newState;
+}
 
 // combine reducers
 
