b63d397ff7feb2ef662dcd72b8b397748ec8f87a
Joe McWhorter <joseph.s.mcwhorter@gmail.com>
2020-02-11 13:54:41 -0500

    Adds basic states

--- src/index.js
+++ src/index.js
@@ -1,12 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
+
+import { createStore, combineReducers } from 'redux';
+
+// global action types
+
+// basic states
+
+const bookState = [{
+    title: "The Road",
+    author: "Cormac McCarthy",
+    pageCount: 300
+}]
+
+const musicState = [{
+    title: "Brother",
+    artist: "Murder by Death",
+    songLength: 190
+}
+];
+
+const movieState = [{
+    title: 'The Green Mile',
+    yearReleased: '1997',
+    genre: 'Drama'
+}]
+
+// reducers
+
+// combine reducers
+
+// create store
+
+// add debugging and console logging to store
+
+// add dispatches
